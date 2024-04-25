145d7da5660bff8c980a201561dba9568789a459
alexhDev2808 <colores.2808.8000@gmail.com>
2024-04-25 17:07:05 -0600

    Probando ServicioPeliculaLista

--- src/servicio/ServicioPeliculaLista.java
+++ src/servicio/ServicioPeliculaLista.java
@@ -30,9 +30,25 @@ public class ServicioPeliculaLista implements IServicioPelicula {
         int indice = peliculas.indexOf(pelicula);
 
         if ( indice == -1 ){
-            System.out.println("No se encontro la pelicula");
+            System.out.println("No se encontro la pelicula: " + pelicula.getNombre());
         } else {
             System.out.println("Pelicula encontrada en el indice: " + indice);
         }
+    }
+
+    public static void main(String[] args) {
+        var pelicula1 = new Pelicula("Batman");
+        var pelicula2 = new Pelicula("Superman");
+
+        IServicioPelicula servicioPeliculas = new ServicioPeliculaLista();
+
+        servicioPeliculas.agregarPelicula(pelicula1);
+        servicioPeliculas.agregarPelicula(pelicula2);
+
+        servicioPeliculas.listarPeliculas();
+
+        // se debe implementar metodo equals y hashCode
+        servicioPeliculas.buscarPelicula(new Pelicula("Deadpool"));
+
     }
 }
