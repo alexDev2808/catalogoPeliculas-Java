a503b7199289d16e0a77fbbef9399fdec494345e
alexhDev2808 <colores.2808.8000@gmail.com>
2024-04-25 16:45:33 -0600

    Creacion de la clase ServicioPeliculaLista e implementando interfaz IServicioPelicula

---
+++ src/servicio/ServicioPeliculaLista.java
@@ -0,0 +1,38 @@
+package servicio;
+
+import dominio.Pelicula;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServicioPeliculaLista implements IServicioPelicula {
+
+    private final List<Pelicula> peliculas;
+
+    public ServicioPeliculaLista() {
+        peliculas = new ArrayList<>();
+    }
+
+    @Override
+    public void listarPeliculas() {
+        System.out.println("Listado de peliculas...");
+        peliculas.forEach(System.out::println);
+    }
+
+    @Override
+    public void agregarPelicula(Pelicula pelicula) {
+        peliculas.add(pelicula);
+        System.out.println("Se agrego la pelicula: " + pelicula);
+    }
+
+    @Override
+    public void buscarPelicula(Pelicula pelicula) {
+        int indice = peliculas.indexOf(pelicula);
+
+        if ( indice == -1 ){
+            System.out.println("No se encontro la pelicula");
+        } else {
+            System.out.println("Pelicula encontrada en el indice: " + indice);
+        }
+    }
+}
